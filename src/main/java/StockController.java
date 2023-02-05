40ecc110d50b3b44f029dc40067f6754bded7b0a
Luong <3q04032002@gmail.com>
2023-02-05 16:32:31 +0700

    push

---
+++ src/main/java/StockController.java
@@ -0,0 +1,22 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class StockController {
+
+    @FXML
+    private Button sellButton;
+
+    @FXML
+    private TextField buyTextField;
+
+    @FXML
+    private TextField sellTextField;
+
+    @FXML
+    private TextField priceTextField;
+
+    @FXML
+    private Button buyButton;
+
+}
---
+++ src/main/java/StockTradingSystem.java
@@ -0,0 +1,23 @@
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.application.Application;
+
+public class StockTradingSystem extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("StockTradingSystem.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            System.out.println("An error occurred while loading the FXML file: " + e);
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
