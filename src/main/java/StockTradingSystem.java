dea186c0e8b9be0a7cd51982bf8a285258df5a3f
Luong <3q04032002@gmail.com>
2023-02-05 17:03:39 +0700

    commit

--- src/main/java/StockTradingSystem.java
+++ src/main/java/StockTradingSystem.java
@@ -8,7 +8,7 @@ public class StockTradingSystem extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("/StockTradingSystem.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("StockTradingSystem.fxml"));
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.show();
