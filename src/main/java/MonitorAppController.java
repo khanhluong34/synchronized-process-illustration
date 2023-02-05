3378c7f70602c00a46e94ab1cc9e43c07a9ac600
phamson02 <phamtienson02@gmail.com>
2023-02-05 22:42:37 +0700

    update basic fxml, controller

---
+++ src/main/java/MonitorAppController.java
@@ -0,0 +1,22 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class MonitorAppController {
+
+    @FXML
+    private Text buyText;
+
+    @FXML
+    private Text qtyText;
+
+    @FXML
+    private Text sellText;
+
+    @FXML
+    private Button startBtn;
+
+    @FXML
+    private Text traderText;
+
+}
