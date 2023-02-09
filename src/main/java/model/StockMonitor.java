485800005082b0f4102daf85261a677172f4670b
Luong <3q04032002@gmail.com>
2023-02-09 15:36:50 +0700

    modify StockMonitor

--- src/main/java/model/StockMonitor.java
+++ src/main/java/model/StockMonitor.java
@@ -1,10 +1,14 @@
 package model;
 
+import java.util.LinkedList;
+
 public class StockMonitor {
     private int quantity;
+    private int maxQuantity = 100;
+    private int lock = 1;
     private TradeRequest handlingRequest;
 
-    private TradeRequest[] queuedRequest;
+    private LinkedList<TradeRequest> queuedRequest;
 
     public int getQuantity() {
         return quantity;
@@ -19,37 +23,53 @@ public class StockMonitor {
     }
 
     public synchronized void buy(TradeRequest request) {
-//        handlingRequest = request;
-//        if (this.quantity < request.getQuantity()) {
-//            System.out.println("Not enough stocks available for purchase, " + request.getNameTrader() + " can not execute this transaction");
-//        } else {
-//            try {
-//                Thread.sleep(2000);
-//                System.out.println("Transaction time ...");
-//            } catch (InterruptedException e) {
-//                System.out.println("Thread interrupted: " + e.getMessage());
-//            }
-//            this.quantity -= request.getQuantity();
-//            System.out.println(Thread.currentThread().getName() + " bought " + request.getQuantity() + " stocks successfully");
-//        }
+        handlingRequest = request;
         if (this.quantity < request.getQuantity()) {
-            {
-                try { wait(); }
-                catch (InterruptedException ignored) { }
+            System.out.println("Not enough stocks available for purchase, " + request.getNameTrader() + " can not execute this transaction");
+            try {
+                // wait until enough stock quantity for purchase
+                this.queuedRequest.add(request);
+                wait();
+            } catch (InterruptedException e) {
+                System.out.print(e.getMessage());
             }
-
-        }
+        } else {
+            this.queuedRequest.remove(request);
+            try {
+                Thread.sleep(2000);
+                System.out.println("Transaction time ...");
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted: " + e.getMessage());
+            }
+            this.quantity -= request.getQuantity();
+            System.out.println(Thread.currentThread().getName() + " bought " + request.getQuantity() + " stocks successfully");
+            // notifies anyone of threads waiting in the wait set to wake up arbitrarily.
+            notify();
+            }
     }
 
     public synchronized void sell(TradeRequest request) {
         handlingRequest = request;
-        try {
-            Thread.sleep(2000);
-            System.out.println("Transaction time ...");
-        } catch (InterruptedException e) {
-            System.out.println("Thread interrupted: " + e.getMessage());
+        if (maxQuantity - quantity < request.getQuantity()) {
+            System.out.println("System holds enough stock quantity, preventing selling stock to protect the stock price");
+            try {
+                queuedRequest.add(request);
+                wait();
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        } else {
+            queuedRequest.remove(request);
+            try {
+                Thread.sleep(2000);
+                System.out.println("Transaction time ...");
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted: " + e.getMessage());
+            }
+            this.quantity += request.getQuantity();
+            System.out.println(Thread.currentThread().getName() + " sold " + request.getQuantity() + " stocks successfully");
+            // notifies anyone of threads waiting in the wait set to wake up arbitrarily.
+            notify();
         }
-        this.quantity += request.getQuantity();
-        System.out.println(Thread.currentThread().getName() + " sold " + request.getQuantity() + " stocks successfully");
     }
 }
